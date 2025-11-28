c22e134ed17f88494ef6b5f6657040d4b5bab651
Raul van Dijk Escoriza <r.vandijkescoriza@uva.nl>
2025-11-28 16:34:35 +0100

    Finish game logic

--- main_js.js
+++ main_js.js
@@ -1,6 +1,4 @@
 
-let humanScore = 0;
-let computerScore = 0;
 
 
 // Get random int between 1 and 3
@@ -18,7 +16,7 @@ function getHumanChoice() {
         return choice;
     }
     else {
-        console.warn("Wrong answer buddy. Reply with: Rock, paper or scissors!");
+        console.warn("Wrong answer buddy. Reply with: rock, paper or scissors!");
         return getHumanChoice();
     };
 }
@@ -42,23 +40,48 @@ function playRound(humanChoice, computerChoice) {
 
     if (humanChoice === computerChoice) {
         console.log("It's a draw!");
+        return 0;
     }
     else if ((humanChoice === "rock" && computerChoice === "scissors") ||
              (humanChoice === "paper" && computerChoice === "rock") ||
-             (humanChoice === "scissors" & computerChoice === "paper")) {
-                humanScore += 1;
-                console.log(`You win! ${humanChoice} beats ${computerChoice}`);
-    }
+             (humanChoice === "scissors" && computerChoice === "paper")) {
+                console.log(`You win this round! ${humanChoice} beats ${computerChoice}`);
+                return 1;
+            }   
     else {
-        computerScore += 1;
-        console.log(`You lose! ${computerChoice} beats ${humanChoice}` );
-    }        
-    console.log(`Score:
-    Human -> ${humanScore} || ${computerScore} <- Computer`);
-
+        console.log(`You lose this round! ${computerChoice} beats ${humanChoice}` );
+        return 2;
+    };        
 };
 
-const computerChoice = getComputerChoice();
-//const humanChoice = getHumanChoice();
+function playGame() {
+    let humanScore = 0;
+    let computerScore = 0;
 
-playRound(humanChoice, computerChoice)
+    for (let i = 0; i < 5; i++) {
+        console.log(`ROUND ${i + 1}`);
+        const computerChoice = getComputerChoice();
+        const humanChoice = getHumanChoice();
+        let score = playRound(humanChoice, computerChoice);
+
+        if (score === 1) {
+            humanScore += 1;
+        }
+        else if (score === 2) {
+            computerScore += 1;
+        };
+    };
+
+    if (humanScore < computerScore) {
+        console.log("You lost the game!");
+    }
+    else if (humanScore > computerScore) {
+        console.log("You won the game! Congratulations!");
+    }
+    else {
+        console.log("It's a draw");
+    };
+    console.log(`Final Score: \n Human: ${humanScore} \n Computer: ${computerScore}`);
+}
+
+playGame()
