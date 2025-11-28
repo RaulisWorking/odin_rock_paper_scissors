2861463adc01aeff0ac4f086ef6543613cad9d9d
Raul van Dijk Escoriza <r.vandijkescoriza@uva.nl>
2025-11-28 12:12:36 +0100

    Finish game logic

--- main_js.js
+++ main_js.js
@@ -1,4 +1,8 @@
 
+let humanScore = 0;
+let computerScore = 0;
+
+
 // Get random int between 1 and 3
 function getRandomInt() {
     const minCeiled = Math.ceil(1);
@@ -6,30 +10,52 @@ function getRandomInt() {
     return Math.floor(Math.random() * (maxFloored - minCeiled) + 1);
 }
 
+function getHumanChoice() {
+    let choice = prompt("Rock, Paper or Scissors?", "Rock").toLowerCase();
+
+    if ((choice === "rock") || (choice === "paper") || (choice === "scissors")) {
+        return choice;
+    }
+    else {
+        console.warning("Wrong answer buddy. Reply with: Rock, paper or scissors!");
+        getHumanChoice();
+    };
+
+}
+
 // Prompt user for choice convert it to int
-function getPlayerChoice() {
-    const draw = prompt("Rock, Paper or Scissors?", "Rock").toLowerCase();
+function getComputerChoice() {
+    const draw = getRandomInt();
 
     switch(draw) {
-        case("rock"): 
-            return 1;
-        case("paper"):
-            return 2;
-        case("scissors"):
-            return 3;  
+        case(1): 
+            return "rock";
+        case(2):
+            return "paper";
+        case(3):
+            return "scissors";  
         default:
-            console.error("Wrong answer buddy. Reply with: Rock, paper or scissors!");  
+              
             break;
     };
 };
 
-function playRockPaperScissors() {
-    const playerDraw = getPlayerChoice();
-    const computerDraw = getRandomInt();    
 
-    if (playerDraw === computerDraw) {
+function playRound(humanChoice, computerChoice) {  
+
+    if (humanChoice === computerChoice) {
         console.log("It's a draw!");
-    };
-
+    }
+    else if ((humanChoice === "rock" && computerChoice === "scissors") ||
+             (humanChoice === "paper" && computerChoice === "rock") ||
+             (humanChoice === "scissors" & computerChoice === "paper")) {
+                humanScore += 1;
+                console.log(`You win! ${humanChoice} beats ${computerChoice}`);
+    }
+    else {
+        computerScore += 1;
+        console.log(`You lose! ${computerChoice} beats ${humanChoice}` );
+    }        
+    console.log(`Score: \n Human: ${humanScore} ---- Computer ${computerScore}`);
 
 };
